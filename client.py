36e1555fe7bcdf59a9672ce4f013863f12057835
richietaylor <richard@taylorfamily.co.za>
2024-04-29 12:05:13 +0200

    Refined client server

--- client.py
+++ client.py
@@ -1,18 +1,59 @@
 import socket
+import threading
+
+def receive_messages(sock):
+    while True:
+        try:
+            message = sock.recv(1024).decode('utf-8')
+            if message:
+                print("\r" + message + "\nMessage: ", end="")
+        except:
+            print("You have been disconnected from the server")
+            sock.close()
+            break
+
+def send_messages(sock):
+    while True:
+        message = input("Message: ")
+        try:
+            sock.send(message.encode('utf-8'))
+        except:
+            print("Failed to send message")
+            sock.close()
+            break
 
 def main():
-    host = 'localhost'
-    port = 3000
+    host = '127.0.0.1'
+    port = 12345
     
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
     
-    while True:
-        message = input("Message: ")
-        client_socket.send(message.encode('utf-8'))
-        # Receiving the broadcast message from the server
-        server_message = client_socket.recv(1024).decode('utf-8')
-        print(f"From server: {server_message}")
+    thread_receive = threading.Thread(target=receive_messages, args=(client_socket,))
+    thread_receive.start()
+
+    thread_send = threading.Thread(target=send_messages, args=(client_socket,))
+    thread_send.start()
 
 if __name__ == "__main__":
     main()
+
+
+# import socket
+
+# def main():
+#     host = 'localhost'
+#     port = 3000
+    
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect((host, port))
+    
+#     while True:
+#         message = input("Message: ")
+#         client_socket.send(message.encode('utf-8'))
+#         # Receiving the broadcast message from the server
+#         server_message = client_socket.recv(1024).decode('utf-8')
+#         print(f"From server: {server_message}")
+
+# if __name__ == "__main__":
+#     main()
--- server.py
+++ server.py
@@ -6,8 +6,8 @@ def client_handler(connection, address, clients):
         try:
             message = connection.recv(1024).decode('utf-8')
             if message:
-                print(f"{address} says: {message}")
-                broadcast(message, connection, clients)
+                print(f"Received from {address}: {message}")
+                broadcast(f"{address} says: {message}", clients)
             else:
                 remove_connection(connection, clients)
                 break
@@ -15,21 +15,22 @@ def client_handler(connection, address, clients):
             remove_connection(connection, clients)
             break
 
-def broadcast(message, connection, clients):
+def broadcast(message, clients):
     for client in clients:
-        if client != connection:
-            try:
-                client.send(message.encode('utf-8'))
-            except:
-                remove_connection(client, clients)
+        try:
+            client.send(message.encode('utf-8'))
+        except:
+            remove_connection(client, clients)
 
 def remove_connection(connection, clients):
     if connection in clients:
         clients.remove(connection)
+        connection.close()
+        print(f"Connection closed with {connection}")
 
 def main():
-    host = 'localhost'
-    port = 3000
+    host = '127.0.0.1'
+    port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
@@ -41,7 +42,6 @@ def main():
     while True:
         connection, address = server_socket.accept()
         print(f"Connected with {address}")
-
         clients.append(connection)
         
         thread = threading.Thread(target=client_handler, args=(connection, address, clients))
@@ -49,3 +49,55 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# import socket
+# import threading
+
+# def client_handler(connection, address, clients):
+#     while True:
+#         try:
+#             message = connection.recv(1024).decode('utf-8')
+#             if message:
+#                 print(f"{address} says: {message}")
+#                 broadcast(message, connection, clients)
+#             else:
+#                 remove_connection(connection, clients)
+#                 break
+#         except:
+#             remove_connection(connection, clients)
+#             break
+
+# def broadcast(message, connection, clients):
+#     for client in clients:
+#         if client != connection:
+#             try:
+#                 client.send(message.encode('utf-8'))
+#             except:
+#                 remove_connection(client, clients)
+
+# def remove_connection(connection, clients):
+#     if connection in clients:
+#         clients.remove(connection)
+
+# def main():
+#     host = 'localhost'
+#     port = 3000
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind((host, port))
+#     server_socket.listen()
+    
+#     clients = []
+
+#     print(f"Server is listening on {host}:{port}")
+
+#     while True:
+#         connection, address = server_socket.accept()
+#         print(f"Connected with {address}")
+
+#         clients.append(connection)
+        
+#         thread = threading.Thread(target=client_handler, args=(connection, address, clients))
+#         thread.start()
+
+# if __name__ == "__main__":
+#     main()
