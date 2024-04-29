ebfb7e88245984a45503d2b1a39c962f8a182462
richietaylor <richard@taylorfamily.co.za>
2024-04-29 13:42:03 +0200

    Server and client connection

--- client.py
+++ client.py
@@ -23,19 +23,33 @@ def send_messages(sock):
             break
 
 def main():
-    host = '127.0.0.1'
-    port = 12345
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((host, port))
-    except:
-        print("The server is down")
-    
-    thread_receive = threading.Thread(target=receive_messages, args=(client_socket,))
-    thread_receive.start()
+    while True:
+        if input("S for server or C for client: ") == 'S':    
+            server_ip = '127.0.0.1'
+            server_port = 12345
+            try:
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((server_ip, server_port))
+            except:
+                print("The server is down")
+                continue
+                
+        else:
+            ip = "localhost"
+            port = 3000
+            try:
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((ip, port))
+            except:
+                print("This client is offline")
+                continue
+                
 
-    thread_send = threading.Thread(target=send_messages, args=(client_socket,))
-    thread_send.start()
+        thread_receive = threading.Thread(target=receive_messages, args=(client_socket,))
+        thread_receive.start()
+
+        thread_send = threading.Thread(target=send_messages, args=(client_socket,))
+        thread_send.start()
 
 if __name__ == "__main__":
     main()
